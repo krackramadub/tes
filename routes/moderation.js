5b3fbd8ca9b6fbf25aab9ec39accc523cb5742a0
ezreshenie-support <pyzhov.pavel.99@gmail.com>
2019-05-14 20:59:18 +0300

    Move moderator's bagreport code to moderation.js, add output reviews to moderation system

--- routes/moderation.js
+++ routes/moderation.js
@@ -6,6 +6,29 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var query = require('../db');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+//Подключение к базе данных
+var mySqlConfig = {
+  host: 'localhost', //расположение БД
+  user: 'root', //имя пользователя
+  password: '', //пароль
+  database: 'diplom', //название БД
+};
+
+var con = mysql.createConnection(mySqlConfig);
+con.connect(function (err) {
+    if (!err) {
+        console.log("DB Connected.")
+    } else {
+        console.log("DB error.")
+    }
+
+});
 
 module.exports = router;
 
@@ -13,25 +36,47 @@ module.exports = router;
 console.log("[MODERATION] Module connected successfuly!");
 
 //Подключение главной страницы личного кабинета системы модерации
-router.get('/moderation', function(req,res){
+router.get('/moderation', function(req,res,next){
     var data = req.data;
     //Проверяем роль пользователя в целях защиты от несанкционированного доступа
     //В случае входа пользователя с ролью поддержки перенаправляем его на соответствующую страницу
     if(data.user.roles == "moder"){
-        data.title = "Система модерации ''ezReshenie''";
-        res.render('moderation', data);
-        //Выводим в консоль сообщение об успешном входе
-        console.log("[MODERATION] User", data.user.username, "login successfully!");
-    }
+        data.title = "Система модерации ''ezReshenie''";        
+        //Запросы отзывов из базы данных (1 запрос - ожидающие модерацию, 2 запрос - отклоненные)
+        data.reviews=[];
+        data.rdenied=[];
+        con.query('SELECT * from review WHERE status=0', false, function (err, rows, fields) {
+          if (!err)
+              console.log('[MODERATION] Pending reviews: ' + rows.length);
+          if (rows) {
+              for (var i = 0; i < rows.length; i++) {
+                  for (var i in rows) {
+                      data.reviews.push(JSON.stringify(rows[i]))
+                  }
+              }
+          }
+          con.query('SELECT * from review WHERE status=2', false, function (err, rows, fields) {
+              if (!err)
+                  console.log('[MODERATION] Denied reviews : ' + rows.length);
+              if (rows) {
+                  for (var i = 0; i < rows.length; i++) {
+                      for (var i in rows) {
+                          data.rdenied.push(JSON.stringify(rows[i]));
+                      }
+                  }
+              }
+            });
+            res.render('moderation', data);
+            //Выводим в консоль сообщение об успешном входе
+            console.log("[MODERATION] User", data.user.username, "login successfully!");
+          });
     //При попытке несанкционированного доступа сообщаем об отсутствии прав на доступ к странице
-    else{
+   } else{
          res.render('denied');
          //В консоль сервера выводим сообщение о попытке несанкционированного доступа
          console.log("[MODERATION] Access for", data.user.username, "denied!");
     }
 });
-
-module.exports = router
 
 //Обработчик блокировки (бана)
 router.get('/banned', function(req,res){
@@ -44,70 +89,18 @@ router.get('/banned', function(req,res){
     console.log("[User control system] This user is banned!");
 });
 
+//Отправка отчета об ошибках
+router.post('/minsertbugs', function (req, res, next) {
+var data = req.data;
+var title = req.body.postTitle;
+var content = req.body.postText;
+var username = req.body.user;
+var sql = "INSERT INTO bugreport (username, title, text) VALUES (?, ?, ?)";
+query(sql, [username, title, content], function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+});
+res.redirect('back');
+});
 
-router.get('/moderation/work', function(req, res, next) {
-    //Задаем необходимые переменные. В данном случае - заголовок страницы и 2 массива для хранения данных
-    // var data = req.data;
-    // data.title = "ezReshenie"
-    // data.support = [];
-    // data.support_inwork = [];
 
-//Подключение к базе данных
-var mySqlConfig = {
-    host: 'localhost', //расположение БД
-    user: 'root', //имя пользователя
-    password: '', //пароль
-    database: 'diplom', //название БД
-};
-
-//Обращение к базе данных
-var con = mysql.createConnection(mySqlConfig)
- con.connect(function(err){
-   if(!err){
-     console.log("DB Connected.")
-   }else{
-     console.log("DB error.")
-   };
- })
-
-//  //Выполнение запросов к БД для получения хранящихся в ней заявок
-//  //Сначала выполняем запрос необработанных заявок (status = 0)
-//  con.query('SELECT * from diplom.support WHERE status=0',function(err, rows, fields) {
-//        if (!err)
-//          //Проверяем выполнение запроса. Если он выполнен успешно, выводим сообщение с количеством записей
-//          console.log('The solution is: ' + rows.length);
-//        else
-//          //В случае ошибки выводим в консоль сообщение
-//          console.log('Error while performing Query.');
-//          //
-//          if(rows != ""){
-//           for(var i =0; i < rows.length;i++){
-//              for(var i in rows){
-//               data.support.push(JSON.stringify(rows[i]))
-//             }   
-//           }
-//         }
-//         //Если необработанных заявок нет, то выводим сообщение в консоль
-//         else{
-//           console.log("Support data with STATUS = 0 not found!");
-//         }
-//     });
-//     //То же самое, что и в 1 случае, только для обработанных заявок (в случае, если был дан ответ)
-//     //Данный код дублирует предыдущий лишь с той разницей, что выбираются заявки с значением status = 1
-//     con.query('SELECT * from diplom.support WHERE status=1',function(err, rows, fields) {
-//       if (!err)
-//         console.log('The solution is: ' + rows.length);
-//       else
-//         console.log('Error while performing Query.');
-//         if(rows != ""){
-//           for(var i =0; i < rows.length;i++){
-//               for(var i in rows){
-//               data.support_solved.push(JSON.stringify(rows[i]))
-//             }   
-//           }
-//         }
-//         else{
-//           console.log("Support data with STATUS = 1 not found!");
-//         }
-//    })
-})
