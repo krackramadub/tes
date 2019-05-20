9b61dca84912aa00acac81509f0abe32027ce43f
ezreshenie-support <pyzhov.pavel.99@gmail.com>
2019-05-20 07:19:08 +0300

    Save actual moderation system code. With all bugs and testing functions

--- routes/moderation.js
+++ routes/moderation.js
@@ -45,9 +45,9 @@ router.get('/moderation', function(req,res,next){
         //Запросы отзывов из базы данных (1 запрос - ожидающие модерацию, 2 запрос - отклоненные)
         data.reviews=[];
         data.rdenied=[];
-        con.query('SELECT * from review WHERE status=0', false, function (err, rows, fields) {
+        con.query('SELECT * from pageorders WHERE status=0', false, function (err, rows, fields) {
           if (!err)
-              console.log('[MODERATION] Pending reviews: ' + rows.length);
+              console.log('[MODERATION] Pending orders: ' + rows.length);
           if (rows) {
               for (var i = 0; i < rows.length; i++) {
                   for (var i in rows) {
@@ -55,21 +55,27 @@ router.get('/moderation', function(req,res,next){
                   }
               }
           }
-          con.query('SELECT * from review WHERE status=2', false, function (err, rows, fields) {
+          else{
+              console.log('[MODEARTION] Pending orders not found!');
+          }
+          con.query('SELECT * from pageorders WHERE status=2', false, function (err, rows, fields) {
               if (!err)
-                  console.log('[MODERATION] Denied reviews : ' + rows.length);
+                  console.log('[MODERATION] Denied orders : ' + rows.length);
               if (rows) {
                   for (var i = 0; i < rows.length; i++) {
                       for (var i in rows) {
                           data.rdenied.push(JSON.stringify(rows[i]));
                       }
                   }
+              } 
+              else{
+                console.log('[MODERATION] Denied order not found!');
               }
             });
             res.render('moderation', data);
-            //Выводим в консоль сообщение об успешном входе
-            console.log("[MODERATION] User", data.user.username, "login successfully!");
           });
+          //Выводим в консоль сообщение об успешном входе
+          console.log("[MODERATION] User", data.user.username, "login successfully!");
     //При попытке несанкционированного доступа сообщаем об отсутствии прав на доступ к странице
    } else{
          res.render('denied');
@@ -91,16 +97,66 @@ router.get('/banned', function(req,res){
 
 //Отправка отчета об ошибках
 router.post('/minsertbugs', function (req, res, next) {
-var data = req.data;
 var title = req.body.postTitle;
 var content = req.body.postText;
 var username = req.body.user;
 var sql = "INSERT INTO bugreport (username, title, text) VALUES (?, ?, ?)";
-query(sql, [username, title, content], function (err, result) {
+con.query(sql, [username, title, content], function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
 });
 res.redirect('back');
+});
+
+//Обработчик публикации заказов
+router.post('/ordmod', function (req,res,next) {
+    var id = req.body.id;
+    var sql = "UPDATE pageorders SET status = 1 WHERE id = (?)";
+    con.query(sql, [id], function (err, result) {
+        if (err) throw err;
+    });
+    res.redirect('back');
+});
+
+//Обработчик перехода к редактированию заказа
+router.post('/moderation/ordedit', function(req, res, next){
+    res.render('ordedit');
+});
+
+//Обработчик отклонения заказов
+router.post('/orddeny', function (req,res,next) {
+    var id = req.body.id;
+    var sql = "UPDATE pageorders SET status = 2 WHERE id = (?)";
+    con.query(sql, [id], false, function (err, result) {
+        if (err) throw err;
+    });
+    res.redirect('back');
+});
+
+//Обработчик запросов информации о заказах
+router.post('/moderation/orderinfo', function (req,res,next) {
+    var id = req.body.id;
+    var data = req.data;
+    data.title = 'Информация о заказе ID', req.body.id,'';
+    data.ordinfo=[];
+    var sql = "SELECT * FROM pageorders WHERE id = (?)";
+    con.query(sql, [id], function (err, rows, fields) {
+        if (!err)
+            console.log('[MODERATION] Get information about order ID', id, 'successfully!');
+        if (rows) {
+            for (var i = 0; i < rows.length; i++) {
+                for (var i in rows) {
+                    data.ordinfo.push(JSON.stringify(rows[i]));
+                }
+            }
+        }
+        res.render('orderinfo', data);
+    }); 
+});
+
+//Блокировка учетной записи
+router.get('/moderation/ban', function(req, res, next){
+    res.render('ban');
 });
 
 
